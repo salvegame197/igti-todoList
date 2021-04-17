a68f84147808ee192543ccf411725e72bf3b75a3
Gabriel <gabrieldemamann@gmail.com>
2021-04-17 12:00:07 -0300

    Newest

--- git.sh
+++ git.sh
@@ -1,12 +1,11 @@
 PURPLE='\033[0;35m'
 RED='\033[0;31m'
-NC='\033[0m' # No Color
-echo -e "${PURPLE}Mensagem do commit: "
+echo -e "${PURPLE}Mensagem do commit: ${RED}"
 read message
 git add .
 git commit -m "$message"
 git push origin main
-echo -e"${PURPLE}Finalizado"
-echo -e"${RED}Precione qualquer tecla para sair..."
+echo -e "${PURPLE}Finalizado"
+echo -e "${RED}Precione qualquer tecla para sair..."
 read
 
