909eeba96ad877e1fa988d92b1c6fa041e6d8184
Gabriel <gabrieldemamann@gmail.com>
2021-04-17 11:54:34 -0300

    Testing git.sh

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@ echo "Mensagem do commit: "
 read message
 git add .
 git commit -m "$message"
-git push origin master
+git push origin main
 echo "Finalizado"
 echo "Precione qualquer tecla para sair..."
 read
